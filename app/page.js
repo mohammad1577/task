fca455704dba882a5d650bb3bdfd17f4ae6b0827
mohammad1577 <mohammad@Mohammads-MacBook-Pro.local>
2023-12-16 13:57:42 +0330

    change

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Card } from "@/components";
 export default function Home() {
     return (
         <main className="bg-lightGreen-200">
-            <div className="grid grid-cols-3 gap-2 pt-2">
+            <div className="grid grid-cols-4 gap-2 pt-2">
                 <Card />
                 <Card />
                 <Card />
